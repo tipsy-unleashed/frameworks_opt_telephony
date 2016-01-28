4f9c73a4622ccaf523da64be2daa8d8f7ec47bd9
Christopher R. Palmer <crpalmer@gmail.com>
2016-01-28 05:36:49 -0500

    Revert "Fix data call retry issue on PS service loss"
    This reverts commit 4eca1c39056dd248bb6199fdfdb258704e667de2.
    
    Change-Id: Iba8b6cd7720d3605c60edc2f9e8a300608092b91

--- src/java/com/android/internal/telephony/dataconnection/DataConnection.java
+++ src/java/com/android/internal/telephony/dataconnection/DataConnection.java
@@ -1494,13 +1494,12 @@ public class DataConnection extends StateMachine {
                                     + " rat=" + rat + " ignoring");
                         }
                     } else {
-                        if (drs == ServiceState.STATE_IN_SERVICE) {
-                            // have to retry connecting since no attach event will come
-                            if (mConnectionParams.mRetryWhenSSChange) {
-                                retVal = NOT_HANDLED;
-                                break;
-                            }
-                        } else {
+                        // have to retry connecting since no attach event will come
+                        if (mConnectionParams.mRetryWhenSSChange) {
+                            retVal = NOT_HANDLED;
+                            break;
+                        }
+                        if (drs != ServiceState.STATE_IN_SERVICE) {
                             // We've lost the connection and we're retrying but DRS or RAT changed
                             // so we may never succeed, might as well give up.
                             mInactiveState.setEnterNotificationParams(DcFailCause.LOST_CONNECTION);
